1a4b0c13f36c972358933c3ca9499eb9e8cb832c
Jaden Snell <jasn7628@colorado.edu>
2024-04-18 18:04:25 -0600

    updated code

--- noisyLS.py
+++ noisyLS.py
@@ -42,37 +42,41 @@ def perform_and_plot(x_start, x_end):
     y_true = func(x_noisy)
     x_range = np.linspace(x_start, x_end, 100)
 
-    fig, (ax1, ax2,ax3) = plt.subplots(3, 1, figsize=(6, 9), sharex=True)
+    fig, (ax1, ax2, ax3) = plt.subplots(3, 1, figsize=(12, 15))
 
     # Pure quadratic fit ax^2
-    coeffs_quad = least_squares_poly(x_noisy, y_noisy, 2,only_x_squared=True)
-    plot_regression(y_noisy,y_true, x_range, coeffs_quad, 'f1(x) = ax^2',  ax1,ax2,ax3, only_x_squared=True)
+    coeffs_quad = least_squares_poly(x_noisy, y_noisy, 2, only_x_squared=True)
+    plot_regression(y_noisy, y_true, x_range, coeffs_quad, 'f1(x) = ax^2', ax1, ax2, ax3, only_x_squared=True)
 
     # Quadratic fit ax^2 + bx + c
     coeffs_quad_lin = least_squares_poly(x_noisy, y_noisy, 2)
-    plot_regression(y_noisy,y_true, x_range, coeffs_quad_lin, 'f2(x) = ax^2 + bx + c', ax1,ax2,ax3)
+    plot_regression(y_noisy, y_true, x_range, coeffs_quad_lin, 'f2(x) = ax^2 + bx + c', ax1, ax2, ax3)
 
     # 4th degree polynomial fit ax^4 + bx^3 + cx^2 + dx + e
     coeffs_degree4 = least_squares_poly(x_noisy, y_noisy, 4)
-    plot_regression(y_noisy,y_true, x_range, coeffs_degree4, 'f3(x) = ax^4 + bx^3 + cx^2 + dx + e', ax1,ax2,ax3)
+    plot_regression(y_noisy, y_true, x_range, coeffs_degree4, 'f3(x) = ax^4 + bx^3 + cx^2 + dx + e', ax1, ax2, ax3)
 
-    ax1.scatter(x_noisy, y_noisy, color='red', label='Noisy Data')
+    ax1.scatter(x_noisy, y_noisy, color='red', label='Noisy Data', s=5)
     ax1.plot(x_range, func(x_range), label='True Function')
     ax1.set_xlabel('x')
     ax1.set_ylabel('y')
     ax1.set_title('Polynomial Regression Fit')
     ax1.legend()
-    
+
     ax2.set_xlabel('x')
-    ax2.set_ylabel('y')
+    ax2.set_ylabel('Error')
     ax2.set_title('Error for Model')
     ax2.legend()
-    
+
     ax3.set_xlabel('x')
-    ax3.set_ylabel('y')
+    ax3.set_ylabel('Error')
     ax3.set_title('Error for Noise')
     ax3.legend()
-    
+
+    ax1.set_xticks(np.arange(x_start, x_end + 1, 1))
+    ax2.set_xticks(np.arange(x_start, x_end + 1, 1))
+    ax3.set_xticks(np.arange(x_start, x_end + 1, 1))
+
     plt.show()
 
 # Perform regression on the interval [0, 5]
